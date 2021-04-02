f77df66637093d073ec82e2b48c18ae8d8f9adc4
Matthew David Daunt <mdd423@log-1.nyu.cluster>
2021-04-01 21:22:29 -0400

    velocity vs loss plotting works

--- plotvelocityscript.py
+++ plotvelocityscript.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib; matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import astropy.table as at
 import jax.numpy as jnp
@@ -10,46 +11,42 @@ import dataset   as wobble_data
 
 import scipy.constants as const
 import astropy.units as u
+import os.path as path
 
-def plot_model(model,i):
-    plt.plot(model.x - model.shifted[i],model.params,'.r',linestyle='solid',linewidth=.8,zorder=2,alpha=0.5,ms=6)
-
-
-def plot(loss_array,shifts,real_vels):
-    size_x, size_y = wobble_data.getPlotSize(model.epoches)
+def plot(loss_array,shifts,real_vels,model):
+    epoches = real_vels.shape[0]
+    size_x, size_y = wobble_model.getPlotSize(epoches)
 
     fig = plt.figure(figsize=[12.8,9.6])
     # Once again we apply the shift to the xvalues of the model when we plot it
-    for i in range(model.epoches):
+    for i in range(epoches):
         ax = fig.add_subplot(size_x,size_y,i+1)
         ax.set_title('epoch %i: vel %.2f' % (i, model.shifted[i]))
 
-        # plot_model(model,i)
         plt.plot(shifts,loss_array[i,:],'.k',zorder=1,alpha=0.9,ms=6)
-        plt.vlines(real_vel[i])
-        if xlim is None:
-            plt.xlim(min(model.xs[i,:]),max(model.xs[i,:]))
-        else:
-            plt.xlim(xlim[0],xlim[1])
-        plt.ylim(-0.8,0.2)
+        plt.vlines(real_vels[i],ymin=-1000,ymax=1000)
+        plt.ylim(min(loss_array[0,:]),max(loss_array[0,:]))
 
-def plotloss(shifts,model,loss):
-    loss_arr = np.empty((model.epoches,shifts.shape[0]))
+def getloss(shift_grid,model,loss):
+    loss_arr = np.empty((model.epoches,shift_grid.shape[0]))
     for i in range(model.epoches):
-        for j in range(shifts.shape[0]):
-            loss_arr[i,j] = loss(model.params,model.ys[i,:],model.xs[i,:]+shifts[j],None,model)
+        for j,shift in enumerate(shift_grid):
+            loss_arr[i,j] = loss(model.params,model.ys[i,:],model.xs[i,:]+shift,None,model)
     return loss_arr
 
 
 def main():
-    velocities = np.linspace(-const.c/10,const.c/10,4500)/1e3 * u.km/u.s
+    velocities = np.linspace(-300,300,100) * u.km/u.s
     shifts     = wobble_data.shifts(velocities)
 
     loss  = wobble_loss.L2Loss()
-    model = wobble_model.load_model('models/modeln512_l0_r400_mI4f.pt')
-    loss_array = plotloss(shifts,model,loss)
-    plot(loss_array,shifts,model.shifted)
-    plt.show()
+    model_name  = '/home/mdd423/wobble_jax/models/modeln7000_l0_r6000_mI32f.pt'
+    model_tail = path.split(model_name)[1]
+    print(path.split(model_name))
+    model = wobble_model.load_model(model_name)
+    loss_array = getloss(shifts,model,loss)
+    plot(loss_array,shifts,model.shifted,model)
+    plt.savefig('/home/mdd423/wobble_jax/out/velvslossplot_{}.pdf'.format(model_tail))
 
 if __name__ == '__main__':
     main()
