ec8cd54cf43030a2de00d5d4836564fe1316d46f
MattD <matthewdaunt@obsolete.lan>
2021-05-05 19:09:19 -0400

    training update

---
+++ scripts/training.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib; matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import astropy.table as at
+import jax.numpy as jnp
+
+import ..loss      as wobble_loss
+import ..simulator as wobble_sim
+import ..model     as wobble_model
+import ..dataset   as wobble_data
+import ..plottings as wobble_plot
+
+import argparse
+import os.path as path
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-l',action='store',default=0,type=int)
+parser.add_argument('-r',action='store',default=200,type=int)
+parser.add_argument('-n',action='store',default=256,type=int)
+parser.add_argument('--sigma',action='store',default=80.0,type=float)
+parser.add_argument('--maxiter',action='store',default=4,type=int)
+parser.add_argument('--maxiter2',action='store',default=4,type=int)
+parser.add_argument('-f',action='store',required=True)
+parser.add_argument('--dir',action='store',required=True)
+args   = parser.parse_args()
+
+# @profile
+def main():
+    outdir = args.dir
+    os.mkdir(outdir)
+
+    file_tail = path.split(args.f)[1][:-4]
+    model_name = '../out/model{}n{}_l{}_r{}f.pt'.format(file_tail,args.n,args.l,args.r)
+    model_tail = path.split(model_name)[1][:-3]
+
+    tbl     = at.QTable.read(args.f)
+    dataset = wobble_data.AstroDataset(tbl['flux'],tbl['wavelength'],tbl['mask'],tbl['flux_err'])
+    dataset.interpolate_mask()
+    filtered = dataset.gauss_filter(sigma=args.sigma)
+    x, y, y_err = dataset.get_xy(filtered)
+
+    x_shifts = wobble_data.getInitXShift(tbl['BJD'],'HAT-P-20','APO')
+
+    loss = wobble_loss.ChiSquare()
+
+    model  = wobble_model.JnpLinErr(args.n,y[args.l:args.r],x[args.l:args.r],y_err[args.l:args.r],x_shifts)
+    model.optimize(loss,maxiter=args.maxiter)
+
+    # model.plot(xlim=(9.455,9.456))
+    # plt.savefig(outdir + )
+
+    wobble_model.save_model('../models/model{}n{}_l{}_r{}_mI{}f.pt'.format(file_tail,args.n,args.l,args.r,args.maxiter),model)
+
+    velocity_grid = np.linspace(-300,300,100) * u.km/u.s
+    shift_grid    = wobble_data.shifts(velocity_grid)
+    loss_array    = wobble_plot.get_loss_array(velocity_grid,model,loss)
+
+    x_min   = wobble_plot.get_parabolic_min(loss_array,shift_grid)
+
+    model_2 = wobble_model.JnpVelLinErr(args.c,y[args.l:args.r],x[args.l:args.r],y_err[args.l:args.r],x_min,pretrained=model)
+    results = model_2.optimize(loss,maxiter=args.maxiter2)
+
+    model_2.plot(xlim=(9.455,9.456))
+    plt.savefig('../out/{}')
+    model_2.save_model(model_2_name)
+
+
+
+if __name__ == '__main__':
+    main()
