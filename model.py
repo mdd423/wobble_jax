52d4803f8046b1fe924ca2eed296b6fbbbf6add3
MattD <matthewdaunt@10-17-81-65.dynapool.wireless.nyu.edu>
2021-07-15 15:15:35 -0400

    using model.bool to fit or fix model parameters

--- model.py
+++ model.py
@@ -106,6 +106,17 @@ class Model:
     def evaluate(self,x):
         return self(self.p,x,i=None)
 
+    def fix_parameters(self):
+        self.p = np.empty(shape=(0))
+        self.bool = False
+
+    def fit_parameters(self,parameters):
+        self.bool = True
+        if self.p.shape[0] == 0:
+            self.p = parameters
+        else:
+            self.p = np.concatenate((self.p,parameters))
+
 class CompositeModel(Model):
     def __init__(self,models):
         self.models = models
@@ -119,6 +130,9 @@ class CompositeModel(Model):
             indices = np.arange(np.sum(self.parameters_per_model[:k]),np.sum(self.parameters_per_model[:k+1]),dtype=int)
             input = model(p[indices],x,i,*args)
         return input
+
+    def __getitem__(self,idx):
+        return self.models[idx]
 
     def composite(self,x):
         if isinstance(x,CompositeModel):
@@ -135,6 +149,16 @@ class CompositeModel(Model):
             except NameError:
                 pass
             model.p = p[indices]
+
+    def fix_parameters(self,model_index):
+        self.models[model_index].fix_parameters()
+        self.p = np.concatenate([model.p for model in self.models])
+        self.parameters_per_model[model_index] = 0
+
+    def fit_parameters(self,model_index,parameters):
+        self.models[model_index].fit_parameters(parameters)
+        self.p = np.concatenate([model.p for model in self.models])
+        self.parameters_per_model[model_index] = parameters.shape[0]
 
 class AdditiveModel(Model):
 
@@ -160,9 +184,12 @@ class AdditiveModel(Model):
 
     def __radd__(self,x):
         if isinstance(x,CompositeModel):
-            return AdditiveModel(models=[*self.models,*x.models],parameters_per_model=[*self.parameters_per_model,*x.parameters_per_model])
+            return AdditiveModel(models=[*self.models,*x.models])
         else:
-            return AdditiveModel(models=[*self.models,x],parameters_per_model=[*self.parameters_per_model,x.p.shape[0]])
+            return AdditiveModel(models=[*self.models,x])
+
+    def __getitem__(self,idx):
+        return self.models[idx]
 
     def unpack(self,p):
 
@@ -174,16 +201,24 @@ class AdditiveModel(Model):
                 pass
             model.p = p[indices]
 
-class DataCalibration(Model):
+    def fix_parameters(self,model_index):
+        self.models[model_index].fix_parameters()
+        self.parameters_per_model[model_index] = 0
+
+    def fit_parameters(self,model_index,parameters):
+        self.models[model_index].fit_parameters(parameters)
+        self.parameters_per_model[model_index] = parameters.shape[0]
+
+# class DataCalibration(Model):
     def __init__(self,n):
         self.n   = n
         self.delta = np.zeros(n)
         self.func_evals = []
 
-        self.p   = self.delta
+        self.bool = False
 
     def __call__(self,p,x,i,*args):
-
+        p[~self.bool] = self.p[~self.bool]
         y = x + p[i]
         return y
 
@@ -193,138 +228,194 @@ class DataCalibration(Model):
 class ConvolutionalModel(Model):
     def __init__(self,n):
         self.omega = np.ones(n)
-        self.p     = self.omega
+        self.bool = False
+        self.p = np.empty(shape=(0))
 
         self.func_evals = []
 
     def __call__(self,p,x,i):
-        y = signal.convolve(x,p,mode='same')
+        if self.bool:
+            y = signal.convolve(x,p,mode='same')
+        else:
+            y = signal.convolve(x,self.omega,mode='same')
         return y
 
     def unpack(self,p):
         self.omega = p
+
+class ShiftingModel(Model):
+    def __init__(self,deltas):
+        self.epoches = deltas.shape[0]
+        self.deltas = deltas
+        self.bool = False
+        self.p = np.empty(shape=(0))
+
+    def __call__(self,p,x,i):
+        if self.bool:
+            return p[i] + x
+        else:
+            return self.deltas[i] + x
+
+    def unpack(self,p):
+        self.m = p
+
+class StretchingModel(Model):
+    def __init__(self,m=None,epoches=0):
+        self.epoches = stretches.shape[0]
+        if m is None:
+            self.m = np.ones((epoches))
+        else:
+            self.m = m
+        self.bool = False
+        self.p = np.empty(shape=(0))
+
+    def __call__(self,p,x,i):
+        if self.bool:
+            return p[i] * x
+        else:
+            return self.m[i] * x
+
+    def unpack(self,p):
+        self.m = p
+
+class JaxLinear(Model):
+    def __init__(self,xs):
+        self.n       = len(xs)
+        # when defining ones own model, need to include inputs as xs, outputs as ys
+        # and __call__ function that gets ya ther, and params (1d ndarray MUST BE BY SCIPY) to be fit
+        # also assumes epoches of data that is shifted between
+        self.theta = np.zeros(self.n)
+        self.xs    = xs
+
+        self.func_evals = []
+        self.bool = False
+        self.p = np.empty(shape=(0))
+
+    def __call__(self,p,x,i):
+        if self.bool:
+            y = jax.numpy.interp(x, self.xs, p)
+        else:
+            y = jax.numpy.interp(x, self.xs, self.theta)
+        return y
+
+    def unpack(self,p):
+        self.theta = p
 
 # foo = jax.numpy.interp(xs, x - shifts, params)
 # res = scipy.optimize.minimize(lamdba(): (ys - foo(xs))**2, params, args=(self,*args), method='BFGS', jac=jax.grad(loss),
 #        options={'disp': True})
 
-class TelluricModel(Model):
-    def __init__(self,x_grid,airmass):
-        self.n = x_grid.shape[0]
-        self.x = x_grid
-        self.omega = np.zeros(self.n)
-        self.airmass = airmass
-
-        self.p = self.omega
-        self.func_evals = []
-
-    def __call__(self,p,x,i,*args):
-        y = self.airmass[i] * jax.numpy.interp(x, self.x, p)
-        return y
-
-    def unpack(self,p):
-        self.omega = p
-
-class GasCellModel(Model):
-    def __init__(self,lines,widths):
-        self.n      = lines.shape[0]
-        self.lines  = lines
-        self.widths = widths
-        self.b      = np.ones(self.n)
-
-        self.func_evals = []
-        self.p = self.b
-
-    def __call__(self,p,x,i,*args):
-
-        y = np.zeros(x.shape)
-        for k,amp in enumerate(p):
-            y -= amp * np.exp(-np.power(x - self.lines[k], 2.) / (2 * np.power(self.widths[k], 2.)))
-        return y
-
-    def unpack(self,p):
-        self.b = p
-
-class LinearModel(Model):
-    def __init__(self,x_grid,delta):
-        self.epoches = len(delta)
-        self.n = len(x_grid)
-        # when defining ones own model, need to include inputs as xs, outputs as ys
-        # and __call__ function that gets ya ther, and params (1d ndarray MUST BE BY SCIPY) to be fit
-        # also assumes epoches of data that is shifted between
-        self.omega = np.zeros(self.n)
-        self.delta = delta
-        self.x     = x_grid
-        self.func_evals = []
-
-        self.p     = self.omega
-
-    def __call__(self,p,x,i,*args):
-        # print(type(self),type(x),type(i))
-        # can only be used once model is optimized
-        # i = args[0]
-        # cell_array = self.cell_array[epoch_idx,:] #getCellArray(self.x + self.shifted[epoch_idx],input)
-        cell_array = self.cell_array[i,:] #getCellArray(self.x + self.shifted[epoch_idx],input)
-        # if cell_array is None:
-        #     cell_array = getCellArray(self.x,x)
-        x = x + self.delta[i]
-        y = p
-        # the x values for the model need to be shifted here but only for the intercept
-        m  = (y[cell_array] - y[cell_array-1])/(self.x[cell_array] - self.x[cell_array-1])
-        ys = y[cell_array-1] + m * (x - self.x[cell_array-1])
-        return jnp.array(ys)
-
-    def unpack(self,p):
-        self.omega = p
-
-class JaxLinear(LinearModel):
-
-    def __call__(self,p,x,i=None,*args):
-        if i == None:
-            y = jax.numpy.interp(x, self.x, p)
-        else:
-            y = jax.numpy.interp(x, self.x - self.delta[i], p)
-        return y
-
-    def unpack(self,p):
-        self.omega = p
-
-class JaxVelLinear(LinearModel):
-    def __init__(self,x_grid,delta,p=None):
-        super(JaxVelLinear,self).__init__(x_grid,delta)
-        if p is not None:
-            self.p = p
-        self.p = np.concatenate((self.p,self.delta))
-
-    def __call__(self,p,x,i=None,*args):
-        if i is None:
-            y = jax.numpy.interp(x, self.x, p[:-self.epoches])
-        else:
-            y = jax.numpy.interp(x, self.x - p[-self.epoches+i], p[:-self.epoches])
-        return y
-
-    def unpack(self,p):
-        self.omega = p[:-self.epoches]
-        self.delta = p[-self.epoches:]
-
-#for future
-class FourierModel(Model):
-    def __init__(self,n,y,x,shifts):
-        self.epoches = y.shape[0]
-        self.ys = y
-        # self.xs = x
-
-        self.base_freq = (x.min() - x.max())/2
-
-        self.shifted = shifts
-
-        self.p = np.zeros(n)
-
-    def __call__(self,p,input,epoch_idx,*args):
-        out = 0
-        for j, param in enumerate(p):
-            if j % 2 == 0:
-                out += param * np.cos((self.base_freq * np.floor(j/2)) * (input + self.shifted[epoch_idx]))
-            if j % 2 == 1:
-                out += param * np.sin((self.base_freq * np.floor(j/2)) * (input + self.shifted[epoch_idx]))
-        return  out
+# class TelluricModel(Model):
+#     def __init__(self,x_grid,airmass):
+#         self.n = x_grid.shape[0]
+#         self.x = x_grid
+#         self.omega = np.zeros(self.n)
+#         self.airmass = airmass
+#
+#         self.p = self.omega
+#         self.bool = np.ones(self.p.shape,dtype='bool')
+#         self.func_evals = []
+#
+#     def __call__(self,p,x,i,*args):
+#         p[~self.bool] = self.p[~self.bool]
+#         y = self.airmass[i] * jax.numpy.interp(x, self.x, p)
+#         return y
+#
+#     def unpack(self,p):
+#         self.omega = p
+#
+# class GasCellModel(Model):
+#     def __init__(self,lines,widths):
+#         self.n      = lines.shape[0]
+#         self.lines  = lines
+#         self.widths = widths
+#         self.b      = np.ones(self.n)
+#
+#         self.func_evals = []
+#         self.p = self.b
+#         self.bool = np.ones(self.p.shape,dtype='bool')
+#
+#     def __call__(self,p,x,i,*args):
+#         p[~self.bool] = self.p[~self.bool]
+#         y = np.zeros(x.shape)
+#         for k,amp in enumerate(p):
+#             y -= amp * np.exp(-np.power(x - self.lines[k], 2.) / (2 * np.power(self.widths[k], 2.)))
+#         return y
+#
+#     def unpack(self,p):
+#         self.b = p
+#
+# class LinearModel(Model):
+#     def __init__(self,x_grid,delta):
+#         self.epoches = len(delta)
+#         self.n = len(x_grid)
+#         # when defining ones own model, need to include inputs as xs, outputs as ys
+#         # and __call__ function that gets ya ther, and params (1d ndarray MUST BE BY SCIPY) to be fit
+#         # also assumes epoches of data that is shifted between
+#         self.omega = np.zeros(self.n)
+#         self.delta = delta
+#         self.x     = x_grid
+#
+#         self.func_evals = []
+#         self.p     = self.omega
+#         self.bool = np.ones(self.p.shape,dtype='bool')
+#
+#     def __call__(self,p,x,i,*args):
+#         # print(type(self),type(x),type(i))
+#         # can only be used once model is optimized
+#         # i = args[0]
+#         # cell_array = self.cell_array[epoch_idx,:] #getCellArray(self.x + self.shifted[epoch_idx],input)
+#         cell_array = self.cell_array[i,:] #getCellArray(self.x + self.shifted[epoch_idx],input)
+#         # if cell_array is None:
+#         #     cell_array = getCellArray(self.x,x)
+#         x = x + self.delta[i]
+#         y = p
+#         # the x values for the model need to be shifted here but only for the intercept
+#         m  = (y[cell_array] - y[cell_array-1])/(self.x[cell_array] - self.x[cell_array-1])
+#         ys = y[cell_array-1] + m * (x - self.x[cell_array-1])
+#         return jnp.array(ys)
+#
+#     def unpack(self,p):
+#         self.omega = p
+#
+# class StellarModel(LinearModel):
+#     def __init__(self,x_grid,delta,p=None):
+#         super(StellarModel,self).__init__(x_grid,delta)
+#         if p is not None:
+#             self.p = p
+#         self.p = np.concatenate((self.p,self.delta))
+#         self.bool = np.ones(self.p.shape,dtype='bool')
+#
+#     def __call__(self,p,x,i=None,*args):
+#
+#         if i is None:
+#             y = jax.numpy.interp(x, self.x, p[:-self.epoches])
+#         else:
+#             y = jax.numpy.interp(x, self.x - p[-self.epoches+i], p[:-self.epoches])
+#         return y
+#
+#     def unpack(self,p):
+#         self.omega = p[:-self.epoches]
+#         self.delta = p[-self.epoches:]
+#
+# #for future
+# class FourierModel(Model):
+    # def __init__(self,n,y,x,shifts):
+    #     self.epoches = y.shape[0]
+    #     self.ys = y
+    #     # self.xs = x
+    #
+    #     self.base_freq = (x.min() - x.max())/2
+    #
+    #     self.shifted = shifts
+    #
+    #     self.p = np.zeros(n)
+    #
+    # def __call__(self,p,input,epoch_idx,*args):
+        # out = 0
+        # for j, param in enumerate(p):
+        #     if j % 2 == 0:
+        #         out += param * np.cos((self.base_freq * np.floor(j/2)) * (input + self.shifted[epoch_idx]))
+        #     if j % 2 == 1:
+        #         out += param * np.sin((self.base_freq * np.floor(j/2)) * (input + self.shifted[epoch_idx]))
+        # return  out
