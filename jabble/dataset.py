7233898c3e2636676c2bbe36b389c360d30f58cb
Matthew Daunt <mdd423@nyu.edu>
2023-09-21 16:16:41 -0400

    fix?

--- jabble/dataset.py
+++ jabble/dataset.py
@@ -3,12 +3,6 @@
 import numpy as np
 import numpy.polynomial as polynomial
 
-import astropy.table as at
-import astropy.units as u
-import astropy.coordinates as coord
-import astropy.constants as const
-import astropy.time as atime
-
 import scipy.ndimage
 
 import jax.numpy as jnp
@@ -19,16 +13,6 @@ def find_nearest(array,value):
     idx   = (np.abs(array-value)).argmin()
     return idx
 
-def velocities(shifts):
-    expon = np.exp(2*shifts)
-    vel = const.c * (expon-1)/(1 + expon)
-    return vel
-
-def delta_x(R):
-    return np.log(1+1/R)
-
-def shifts(vel):
-    return np.log(np.sqrt((1 + vel/(const.c))/(1 - vel/(const.c))))
 # important for grid search
 def get_parabolic_min(loss_array,grid,return_all=False):
 
@@ -71,14 +55,9 @@ def get_parabolic_min(loss_array,grid,return_all=False):
         return grid_min
 
 # kinda not important function
-def get_star_velocity(BJD,star_name,observatory_name,parse=False):
-    hatp20_c = coord.SkyCoord.from_name(star_name,parse=parse)
-    loc      = coord.EarthLocation.of_site(observatory_name)
-    ts       = atime.Time(BJD, format='jd', scale='tdb')
-    bc       = hatp20_c.radial_velocity_correction(obstime=ts, location=loc).to(u.km/u.s)
-    return bc
 # important interpolate function
 def interpolate_mask(flux,mask):
+    # assumes even spacing in wavelength of flux
     new_flux = np.zeros(flux.shape)
     new_flux = flux
     for j,mask_row in enumerate(mask):
@@ -87,6 +66,7 @@ def interpolate_mask(flux,mask):
             if mask_ele != 0:
                 cnt += 1
             if mask_ele == 0 and cnt != 0:
+                # right here is that assumption in the linspace wort wavelength
                 new_flux[j,i-cnt:i] = np.linspace(flux[j,i-cnt-1],flux[j,i],cnt+2)[1:-1]
                 cnt = 0
     return new_flux
