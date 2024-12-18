33357a516d3163e3973f7cf0e4469ee8d4207afe
Matthew Daunt <mdd423@nyu.edu>
2024-12-18 11:36:09 -0500

    bug

--- jabble/model.py
+++ jabble/model.py
@@ -447,7 +447,7 @@ class ContainerModel(Model):
     def save_hdf(self,file):
         group = file.create_group(self.__class__.__name__)
         for model in self.models:
-            model.save_hdf(self,group)
+            model.save_hdf(group)
         return group
 
 class CompositeModel(ContainerModel):
