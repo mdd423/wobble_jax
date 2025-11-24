9255916a3b13b249d6e4ce3abe6767c62cf36b70
Matthew Daunt <mdd423@nyu.edu>
2025-11-24 15:47:29 -0500

    save func update for datablock obj

--- jabble/quickplay.py
+++ jabble/quickplay.py
@@ -380,7 +380,7 @@ def get_loss_array(model,datablock,loss,device):
     loss.ready_indices(model)
     loss_array = np.zeros((datablock.datablock['xs'].shape))
     for jjj in range(datablock.datablock['xs'].shape[0]):
-        datarow = datablock.ele(datablock,jjj,device)
+        datarow = datablock.ele(jjj,device)
         # metarow = jabble.loss.dict_ele(metablock,jjj,device)
         loss_array[jjj,:] = loss(model.get_parameters(),datarow,model)
     return loss_array
