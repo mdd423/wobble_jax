0b7ffef7f9722dcb23b36836ba2e0faf4e3d3c43
Matthew Daunt <mdd423@nyu.edu>
2025-11-24 14:58:17 -0500

    save func update for datablock obj

--- jabble/quickplay.py
+++ jabble/quickplay.py
@@ -376,13 +376,13 @@ def get_RV_sigmas(model, dataset, device=None, rv_ind = [0,0]):
         )
         return np.sqrt(1 / f_info) * dvddx
 
-def get_loss_array(model,datablock,metablock,loss,device):
+def get_loss_array(model,datablock,loss,device):
     loss.ready_indices(model)
     loss_array = np.zeros((datablock['xs'].shape))
     for jjj in range(datablock['xs'].shape[0]):
-        datarow = jabble.loss.dict_ele(datablock,jjj,device)
-        metarow = jabble.loss.dict_ele(metablock,jjj,device)
-        loss_array[jjj,:] = loss(model.get_parameters(),datarow,metarow,model)
+        datarow = datablock.ele(datablock,jjj,device)
+        # metarow = jabble.loss.dict_ele(metablock,jjj,device)
+        loss_array[jjj,:] = loss(model.get_parameters(),datarow,model)
     return loss_array
     
 def save(self, filename: str, dataname: str, data, shifts, loss, device, rv_ind) -> None:
@@ -398,10 +398,10 @@ def save(self, filename: str, dataname: str, data, shifts, loss, device, rv_ind)
         jabble.model.save(dataname,data)
         
         with h5py.File(filename + "_RVS.hdf",'w') as file:
-            datablock, metablock, meta_keys = data.blockify(device,return_keys=True)
+            datablock = data.blockify(device,return_keys=True)
             file.create_dataset("RVs",data=jabble.physics.velocities(shifts))
             file.create_dataset("RV_err",data=get_RV_sigmas(self, data, device=device, model=self,rv_ind=rv_ind))
-            file.create_dataset("Times",data=meta_keys['times'])
+            file.create_dataset("Times",data=datablock.meta_keys['times'])
 
             head, tail = os.path.split(filename)
             file.attrs['model'] = filename
